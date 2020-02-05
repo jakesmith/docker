a511ad55c65c2214eab7c7a50feb9b0468eff361
Jake Smith <jake.smith@lexisnexisrisk.com>
2020-02-05 12:41:44 +0000

    wip

--- hpcc/hpccbuildfromsrc/hpccbuild-install/buildscript.sh
+++ hpcc/hpccbuildfromsrc/hpccbuild-install/buildscript.sh
@@ -7,4 +7,6 @@ rm -f *.deb
 cmake -DRUNTIME_USER=build -DDESTDIR=$(realpath ~build)/hpccinstall -D ECLWATCH_BUILD_STRATEGY="IF_MISSING" -D USE_CPPUNIT=On -D CMAKE_BUILD_TYPE=Debug -D USE_LIBMEMCACHED=Off ../HPCC-Platform
 numcores=$(grep -c ^processor /proc/cpuinfo)
 let "numthreads = $numcores * 3 / 4"
+
+echo make -j ${numthreads} install
 make -j ${numthreads} install
